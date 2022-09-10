b13bb4fb2814e32feb0d2d84a71e1ec99f4ca8bc
abdullahalmamoon <abdullahalmamoon9@gmail.com>
2022-09-10 13:32:52 +0600

    added template.js

---
+++ js/template.js
@@ -0,0 +1,26 @@
+const person = 'Adam Smith';
+const person2 = "Ben White";
+const person3 = `Donald Trump`;
+
+const multiLine = 'Frist Line Text \n' +
+    'Second Line of Text \n' +
+    'Third Line of Text \n' +
+    'Fourth Line of String';
+
+// console.log(multiLine);
+
+const newMultiLine = `First Line of Text
+Second Line of Text
+Third Line of code
+Fourth Line of String`;
+
+// console.log(newMultiLine);
+
+const a = 30;
+const b = 40;
+const nums = [34, 34, 21, 64, 3, 86];
+const summary = ' sum of: ' + a + ' and ' + b + ' is: ' + (a + b);
+// console.log(summary);
+
+const newSummary = ` sum of ${a} and ${b} is ${a + b}`;
+console.log(newSummary);
